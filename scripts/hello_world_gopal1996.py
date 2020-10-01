9b63a79b73b0921c5f68fc24d3ad7a8bcc9a3282
Gopalakrishnan <krishnan.gopal34@gmail.com>
2020-10-02 01:10:47 +0530

    comment added

--- scripts/hello_world_gopal1996.py
+++ scripts/hello_world_gopal1996.py
@@ -1,6 +1,6 @@
-// LANGUAGE: Python
-// ENV: Python
-// AUTHOR: Gopalakrishnan C
-// GITHUB: https://github.com/gopal1996
+# LANGUAGE: Python
+# ENV: Python
+# AUTHOR: Gopalakrishnan C
+# GITHUB: https://github.com/gopal1996
 
 print("Hello World!! This is my first contribution!! Hurray :)")
